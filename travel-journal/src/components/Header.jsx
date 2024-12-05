ae82566b4846860af24fe7973e44c24fc23c9cb9
Sebastian Quiros <sebastianq04@gmail.com>
2024-12-04 23:40:38 -0500

    Finished react-facts project and strated travel-journal project

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./src/images/globe.png" alt="globe icon"/>
+                <h1>my travel journal.</h1>
+            </nav>
+        </header>
+    )
+}
